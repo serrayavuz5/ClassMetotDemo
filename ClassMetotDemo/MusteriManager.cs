6ee23bf3c6b683b6c1baf0425f7b35b0c9a3efe8
Serra Yavuz <srryavuz@gmail.com>
2021-01-24 21:26:55 +0300

    Class Ödevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla eklendi. ");
+            Console.WriteLine("Müşteri Tc : " + musteri.Tc);
+            Console.WriteLine("Müşteri Adı : " + musteri.Ad);
+            Console.WriteLine("Müşteri Soyadı : " + musteri.Soyad);
+            Console.WriteLine("Müşteri Telefon Numarası : " + musteri.TelefonNumarasi);
+            
+        }
+        
+        public  void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silme işlemi başarıyla gerçekleşti. "+ musteri.Tc);
+            
+        }
+
+        public  void MusteriListesi(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listesi ");
+            Console.WriteLine("Müşteri Tc : " + musteri.Tc);
+            Console.WriteLine("Müşteri Adı : " + musteri.Ad);
+            Console.WriteLine("Müşteri Soyadı : " + musteri.Soyad);
+            Console.WriteLine("Müşteri Telefon Numarası : " + musteri.TelefonNumarasi);
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,32 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Tc = 333;
+            musteri1.Ad = "Serra";
+            musteri1.Soyad = "Yavuz";
+            musteri1.TelefonNumarasi = "0536 935 14 01";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Tc = 222;
+            musteri2.Ad = "Burak";
+            musteri2.Soyad = "Yavuz";
+            musteri2.TelefonNumarasi = "0532 342 48 58";
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            Console.WriteLine("*******************");
+            musteriManager.MusteriEkle(musteri2);
+            Console.WriteLine("*******************");
+
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriSil(musteri2);
+            Console.WriteLine("*******************");
+
+            musteriManager.MusteriListesi(musteri1);
+            Console.WriteLine("*******************");
+            musteriManager.MusteriListesi(musteri2);
+            
         }
     }
 }
